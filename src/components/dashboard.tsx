dc6be1f805560c729d945c8443738a9a511104ab
Iskren <iskren.dev@gmail.com>
2023-02-27 17:48:56 +0100

    feat: firebase initial connection

---
+++ src/components/dashboard.tsx
@@ -0,0 +1,14 @@
+
+import styles from "../styles/Home.module.css";
+
+const Dashboard = () => {
+	return (
+		<div className={styles.container}>
+			<main className={styles.main}>
+				<h1>AML UI</h1>
+			</main>
+		</div>
+	);
+};
+
+export default Dashboard;
